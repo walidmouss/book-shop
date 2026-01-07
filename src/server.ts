a2c5f2569650fd809d48f391cc8115d14864cbd5
Walid Moussa <walidmoussa@gmail.com>
2026-01-07 15:32:05 +0200

    Initial commit: setup Hono + TypeScript project

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import { serve } from "@hono/node-server";
+import { app } from "./app.js";
+
+serve({ fetch: app.fetch, port: 3000 }, (info: any) => {
+  console.log(`Server running at http://localhost:${info.port} ^_^`);
+});
