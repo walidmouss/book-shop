7f4d2034d00a7c07e93614df4d9325d0d74d41b4
Walid Moussa <walidmoussa@gmail.com>
2026-01-09 01:17:19 +0200

    feat/ added categories database table schema

---
+++ src/db/categories.ts
@@ -0,0 +1,8 @@
+import { pgTable, serial, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const categories = pgTable("categories", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 100 }).notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
