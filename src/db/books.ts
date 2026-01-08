7d6a070d1f03a3c063605b7e8d8677563c3e90ff
Walid Moussa <walidmoussa@gmail.com>
2026-01-09 01:42:21 +0200

    feat/ added books database table schema

---
+++ src/db/books.ts
@@ -0,0 +1,33 @@
+import {
+  pgTable,
+  serial,
+  varchar,
+  text,
+  integer,
+  timestamp,
+  numeric,
+} from "drizzle-orm/pg-core";
+import { authors } from "./authors";
+import { categories } from "./categories";
+import { users } from "./users";
+import { int } from "zod";
+
+export const books = pgTable("books", {
+  id: serial("id").primaryKey(),
+  title: varchar("title", { length: 255 }).notNull().unique(),
+  description: text("description").notNull(),
+  price: numeric("price", { precision: 10, scale: 2 }).notNull(),
+  thumbnail: varchar("thumbnail", { length: 255 }),
+  author_id: integer("author_id")
+    .notNull()
+    .references(() => authors.id),
+  category_id: integer("category_id")
+    .notNull()
+    .references(() => categories.id),
+
+  creator_id: integer("creator_id")
+    .notNull()
+    .references(() => users.id),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
